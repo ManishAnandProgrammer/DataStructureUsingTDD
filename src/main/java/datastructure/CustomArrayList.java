5bdbf41c3cbb0b1cc3227eccc279e0cbd114839e
Manish Anand <manishanandprogrammer@gmail.com>
2021-04-30 08:20:00 +0530

    starting with custom array list using tdd

---
+++ src/main/java/datastructure/CustomArrayList.java
@@ -0,0 +1,15 @@
+package datastructure;
+
+public class CustomArrayList {
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public boolean add(String manish) {
+        return true;
+    }
+
+    public String get(int i) {
+        return "manish";
+    }
+}
---
+++ src/test/java/datastructure/CustomArrayListTest.java
@@ -0,0 +1,38 @@
+package datastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomArrayListTest {
+
+    @Test
+    @DisplayName("Instance Creation Test")
+    void shouldCreateCustomArrayListObject() {
+        CustomArrayList arrayList = new CustomArrayList();
+        assertNotNull(arrayList, "CustomArrayList Object should created..!");
+    }
+
+    @Test
+    void shouldReturnEmptyTrueIfNoElementAdded() {
+        CustomArrayList arrayList = new CustomArrayList();
+        Assertions.assertTrue(arrayList.isEmpty(), "should return true");
+    }
+
+    @Test
+    void shouldAddElementInList() {
+        CustomArrayList arrayList = new CustomArrayList();
+        assertTrue(arrayList.add("manish"), "Should Able to add Element In List..!");
+    }
+
+    @Test
+    void shouldGetFirstStringAsManishOnFirstGetCall() {
+        CustomArrayList arrayList = new CustomArrayList();
+        arrayList.add("manish");
+        assertEquals("manish", arrayList.get(0), "First Value Should Be Manish..!");
+    }
+
+
+}
