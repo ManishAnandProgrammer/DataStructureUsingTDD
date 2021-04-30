feac8dd33470c92229f05b98551b2556e6695928
Manish Anand <manishanandprogrammer@gmail.com>
2021-04-30 09:00:37 +0530

    removed repeated initialization of custom list in every test method, now initializing it with beforeEach() call

--- src/test/java/datastructure/CustomArrayListTest.java
+++ src/test/java/datastructure/CustomArrayListTest.java
@@ -1,5 +1,6 @@
 package datastructure;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,24 +8,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomArrayListTest {
 
+    private CustomArrayList arrayList;
+
+    @BeforeEach
+    void init() {
+        arrayList = new CustomArrayList();
+    }
+
     @Test
     @DisplayName("Instance Creation Test")
     void shouldCreateCustomArrayListObject() {
-        CustomArrayList arrayList = new CustomArrayList();
         assertNotNull(arrayList, "CustomArrayList Object should created..!");
     }
 
     @Test
     @DisplayName("Should Return True If There is No Element Added In List")
     void shouldReturnEmptyTrueIfNoElementAdded() {
-        CustomArrayList arrayList = new CustomArrayList();
         assertTrue(arrayList.isEmpty(), "should return true");
     }
 
     @Test
     @DisplayName("Should Return False If There is Any Element Present In List")
     void shouldReturnEmptyFalseIfElementAdded() {
-        CustomArrayList arrayList = new CustomArrayList();
         arrayList.add("manish");
         assertFalse(arrayList.isEmpty(), "should return false");
     }
@@ -32,20 +37,17 @@ public class CustomArrayListTest {
     @Test
     @DisplayName("Add Element In List")
     void shouldAddElementInList() {
-        CustomArrayList arrayList = new CustomArrayList();
         assertTrue(arrayList.add("manish"), "Should Able to add Element In List..!");
     }
 
     @Test
     void shouldGetFirstStringAsManishOnFirstGetCall() {
-        CustomArrayList arrayList = new CustomArrayList();
         arrayList.add("manish");
         assertEquals("manish", arrayList.get(0), "First Value Should Be Manish..!");
     }
 
     @Test
     void shouldGetFirstStringAsAnandOnSecondGetCall() {
-        CustomArrayList arrayList = new CustomArrayList();
         arrayList.add("manish");
         arrayList.add("anand");
         assertEquals("anand", arrayList.get(1), "Second Value Should Be Anand..!");
