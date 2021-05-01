a0ab61faaf81a23d164ac8e2afb5b786c5fa3319
Manish Anand <manishanandprogrammer@gmail.com>
2021-05-01 11:30:40 +0530

    Code performance increment by using System.arraycopy insted of for loop

--- src/main/java/datastructure/CustomArrayList.java
+++ src/main/java/datastructure/CustomArrayList.java
@@ -4,13 +4,14 @@ public class CustomArrayList {
     private static final int DEFAULT_INITIAL_CAPACITY = 10;
     private int size;
     private String[] strings = new String[DEFAULT_INITIAL_CAPACITY];
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
 
     public boolean isEmpty() {
         return size == 0;
     }
 
     public boolean add(final String value) {
-        if(strings.length > size) {
+        if(capacity > size) {
             strings[size] = value;
             size++;
             return true;
@@ -22,12 +23,12 @@ public class CustomArrayList {
 
     private void extendCapacity() {
         strings = copyOldArrayValuesInNewBiggerArray();
+        capacity = strings.length;
     }
 
     private String[] copyOldArrayValuesInNewBiggerArray() {
         String[] newArray = new String[strings.length * 2];
-        for(int i=0; i < strings.length; i++)
-            newArray[i] = strings[i];
+        System.arraycopy(strings, 0, newArray, 0, capacity);
         return newArray;
     }
 
