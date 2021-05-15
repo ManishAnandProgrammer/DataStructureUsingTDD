40d057967563e06710b87226e6b1c7369c0baa65
Manish Anand <manishanandprogrammer@gmail.com>
2021-05-15 14:02:09 +0530

    Maze Path Example Added

---
+++ src/main/java/datastructure/recursion/string_questions/MazePath.java
@@ -0,0 +1,23 @@
+package datastructure.recursion.string_questions;
+
+public class MazePath {
+    public static void main(String[] args) {
+        path(0, 0, 2, 2, "");
+    }
+
+    private static void path(int currentRow, int currentColumn,
+                             int destinationRow, int destinationColumn,
+                             String result) {
+
+        if(currentRow > destinationRow || currentColumn > destinationColumn)
+            return;
+
+        if(currentRow == destinationRow && currentColumn == destinationColumn) {
+            System.out.println(result);
+            return;
+        }
+
+        path(currentRow, currentColumn+1, destinationRow, destinationColumn, result+"H");
+        path(currentRow+1, currentColumn, destinationRow, destinationColumn, result+"V");
+    }
+}
