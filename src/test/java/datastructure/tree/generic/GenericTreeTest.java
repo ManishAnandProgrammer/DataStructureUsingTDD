42b2a19636a944045edeac5695ea4fd2af276ad6
Manish Anand <manishanandprogrammer@gmail.com>
2021-05-23 10:15:28 +0530

    refactore test for generic tree

--- src/test/java/datastructure/tree/generic/GenericTreeTest.java
+++ src/test/java/datastructure/tree/generic/GenericTreeTest.java
@@ -1,49 +1,45 @@
 package datastructure.tree.generic;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GenericTreeTest {
+
+    private GenericTree tree;
+
+    @BeforeEach
+    void init() {
+        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
+        tree = new GenericTree(arr);
+    }
 
     @Test
     void displayTree() {
-        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-        GenericTree tree = new GenericTree(arr);
-
         tree.display();
     }
 
     @Test
     @DisplayName("Size Recursive")
     void sizeOfTree() {
-        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-        GenericTree tree = new GenericTree(arr);
-
-        Assertions.assertEquals(12, tree.size());
+        assertEquals(12, tree.size());
     }
 
     @Test
     void cachedSizeAndRecursiveSizeShouldBeSame() {
-        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-        GenericTree tree = new GenericTree(arr);
-
-        Assertions.assertEquals(tree.getSizeFromCached(), tree.size());
+        assertEquals(tree.getSizeFromCached(), tree.size());
     }
 
     @Test
     void max() {
-        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-        GenericTree tree = new GenericTree(arr);
-
-        Assertions.assertEquals(120, tree.maxInTree());
+        assertEquals(120, tree.maxInTree());
     }
 
     @Test
     void heightTest() {
-        int[] arr = {10,20,50,-1,60,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
-        GenericTree tree = new GenericTree(arr);
-
-        Assertions.assertEquals(3, tree.height());
+        assertEquals(3, tree.height());
     }
 }
