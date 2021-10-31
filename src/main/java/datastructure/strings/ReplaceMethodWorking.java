eccb9208a8789cd899ce44c577d1ffea8919fb35
Manish Anand <manishanandprogrammer@gmail.com>
2021-10-31 15:21:04 +0530

    Replace method working of String class

---
+++ src/main/java/datastructure/strings/ReplaceMethodWorking.java
@@ -0,0 +1,9 @@
+package datastructure.strings;
+
+public class ReplaceMethodWorking {
+    public static void main(String[] args) {
+        String animal = "animal";
+        String replaced_a_with_A = animal.replace("a", "A");
+        System.out.println(replaced_a_with_A); // AnimAl
+    }
+}
